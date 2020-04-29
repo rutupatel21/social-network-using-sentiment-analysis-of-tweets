934b2e93f3ba8718dd31a66e4ec5ce797635c2fb
rutupatel21 <rutupatel62@gmail.com>
2020-04-29 13:33:10 +0530

    adding changes

--- graphs/python/twitter_mongo_technology.py
+++ graphs/python/twitter_mongo_technology.py
@@ -14,13 +14,13 @@ import os
 
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key = "BlO5mzzPPAUgL3yeGqZkGinK2"
-consumer_secret = "Z7TI0mHTx8SBUcZiQiw4FPzUR13FKGCVyVgy0W8VWFDQPo3sdW"
+consumer_key = "<consumer_key>"
+consumer_secret = "<consumer_secret>"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token = "197307734-W8ymSKyYlTV5aLoBDrWLFk6sliATJ32a3gIiwpMl"
-access_token_secret = "19xziJrA55IMq3XAzND3nlAsK6TDlikN5uENCDEk2osFm"
+access_token = "<your-access-token>"
+access_token_secret = "<your-access-token-secret>"
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
@@ -29,17 +29,17 @@ class StdOutListener(StreamListener):
     def __init__(self, api):
         self.api = api
         super(tweepy.StreamListener, self).__init__()
-        
+
         # Connecting to MongoDB and use the database twitter.
         self.db = pymongo.MongoClient().twitter
-        
+
     def on_status(self, data):
         #print(data.text)
         analysis = TextBlob(data.text, analyzer=NaiveBayesAnalyzer(), np_extractor= ConllExtractor())
         #print ("Confidence :  Positive score: " ,analysis.sentiment.p_pos*100, "  Negative score: ", analysis.sentiment.p_neg*100 )
         self.db.technology.insert({"name":data.user.name,"text":data.text,"created_at":data.created_at,"positive_score:":analysis.sentiment.p_pos*100,"negative_score:":analysis.sentiment.p_neg*100})
         return True
-        
+
     def on_error(self, status):
         print(status)
 
